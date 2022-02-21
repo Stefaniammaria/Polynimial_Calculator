6025a661b58dc2b38f076367853c4293f05451e0
Stefania-Maria Maracine <maracine_stefania@yahoo.com>
2022-02-21 09:46:08 +0200

    initial commit

---
+++ src/main/java/Monomial.java
@@ -0,0 +1,21 @@
+public class Monomial {
+
+    private int coeficient;
+    private int putere;
+
+    public int getCoeficient() {
+        return coeficient;
+    }
+
+    public void setCoeficient(int coeficient) {
+        this.coeficient = coeficient;
+    }
+
+    public int getPutere() {
+        return putere;
+    }
+
+    public void setPutere(int putere) {
+        this.putere = putere;
+    }
+}
---
+++ src/main/java/Polynomial.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Polynomial {
+
+    ArrayList<Monomial> lista = new ArrayList<Monomial>();
+
+}
